0e3251aa432439d7fa913a2f765814ab71cc03db
e.lazuka <e.lazuka@pixelplex.io>
2022-02-21 15:30:32 +0300

    [RB-15]: styling general scrollbar

--- src/entities/category/model.ts
+++ src/entities/category/model.ts
@@ -3,7 +3,7 @@ export interface Category {
   name: string;
 }
 
-export const useCategories = (): Category[] => ([] || [
+export const useCategories = (): Category[] => ([
   {
     id: 100,
     name: 'Books',
